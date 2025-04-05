be26a5ac4adcc4b6e94f2a27bf1ca9f27c1c845b
Readysoon <p.gallaschik@gmail.com>
2025-04-05 20:50:47 +0200

    adapted 'Get in touch' text

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,8 +60,10 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        I'm currently looking for research or software development opportunities that I can balance
+        alongside writing my bachelor thesis in software engineering, with a particular interest in
+        medical imaging and healthcare technology projects. Whether you have a job opportunity or
+        just want to say hi, I’ll try my best to get back to you!
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
