3577365b0a39c251caf5de98f952a1727146ab7a
Readysoon <p.gallaschik@gmail.com>
2025-04-05 15:52:10 +0200

    CI/CD test2

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,7 +60,7 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
+  const two = <h2 className="big-heading">Brittany Gallaschik.</h2>;
   const three = <h3 className="big-heading">I build medical Software.</h3>;
   const four = (
     <>
