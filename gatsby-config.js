bf0f89ddd0a6f7b4bad92802beb6f83c2b41d9cc
Readysoon <p.gallaschik@gmail.com>
2025-04-05 19:45:25 +0200

    init?

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ module.exports = {
   siteMetadata: {
     title: 'Philipp Gallaschik',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Philipp Gallaschik is a doctor and software engineer who builds innovative medical imaging solutions.',
+    siteUrl: 'https://gallaschik.fly.dev', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Philipp Gallaschik',
+        short_name: 'Philipp Gallaschik',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
